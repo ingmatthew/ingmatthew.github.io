54433261e9d21f9ddc482dedf837f0f70d2a08fa
Matthew Ing <matthewing18@gmail.com>
2020-06-25 20:58:45 -0400

    change food

--- typingtext.js
+++ typingtext.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['food lover', 'stock analyzer', 'developer', 'fitness enthusiast'], 'text',['orange','purple','blue', 'red']);
+consoleText(['food lover', 'stock analyst', 'developer', 'fitness enthusiast'], 'text',['orange','purple','blue', 'red']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
